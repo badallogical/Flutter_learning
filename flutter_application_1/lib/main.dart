9a342c3ce34fb48ebff2b92e7fed01b493dbb767
badallogical <badallearn@gmail.com>
2023-10-06 15:13:03 +0530

    Added List with add button

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,6 +9,7 @@ String img2 = "image/bubble-gum-man-plays-guitar.png";
 String img3 = "image/pablo-musician.png";
 var images = [img1, img2, img3];
 int currentImg = 0;
+int counter = 0;
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -18,6 +19,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  void _showPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Popup Message'),
+          content: const Text('This is a pop-up message.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,18 +53,52 @@ class _MyAppState extends State<MyApp> {
               ),
               centerTitle: true,
             ),
-            body: Center(
-              child: Column(children: [
-                const SizedBox(height: 10),
-                ElevatedButton(
-                    child: const Text("Click me"),
-                    onPressed: () {
-                      setState(() {
-                        currentImg = (currentImg + 1) % images.length;
-                      });
-                    }),
-                Image.asset(images[currentImg])
-              ]),
+            body: SingleChildScrollView(
+              child: Center(
+                child: Column(children: [
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                      child: const Text("Change me"),
+                      onPressed: () {
+                        setState(() {
+                          currentImg = (currentImg + 1) % images.length;
+                        });
+                      }),
+                  Image.asset(images[currentImg]),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          counter++;
+                          if (counter > images.length) {
+                            counter = counter - 1;
+                          }
+                        });
+                      },
+                      child: const Text("Add")),
+                  Column(
+                      children: List.generate(counter, (index) {
+                    return Image.asset(images[index]);
+                  })),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red),
+                          onPressed: () {},
+                          child: const Text("Register")),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green),
+                          onPressed: () {},
+                          child: const Text("Login"))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  )
+                ]),
+              ),
             )));
   }
 }
