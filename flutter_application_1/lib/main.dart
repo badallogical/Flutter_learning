eead40cf883ccea2c918f08516b4c77ac679d236
badallogical <badallearn@gmail.com>
2023-10-07 13:48:46 +0530

    Navigator Added

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_application_1/pages/login.dart";
 
 void main() {
   runApp(const MyApp());
@@ -11,33 +12,8 @@ var images = [img1, img2, img3];
 int currentImg = 0;
 int counter = 0;
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  void _showPopup() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Popup Message'),
-          content: const Text('This is a pop-up message.'),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,60 +21,80 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         title: "Welcome to Flutter",
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                "Welcome to Flutter",
-                textDirection: TextDirection.ltr,
-              ),
-              centerTitle: true,
-            ),
-            body: SingleChildScrollView(
-              child: Center(
-                child: Column(children: [
-                  const SizedBox(height: 10),
+        home: const MyHome());
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHome();
+}
+
+class _MyHome extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Welcome to Flutter",
+            textDirection: TextDirection.ltr,
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(children: [
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  child: const Text("Change me"),
+                  onPressed: () {
+                    setState(() {
+                      currentImg = (currentImg + 1) % images.length;
+                    });
+                  }),
+              Image.asset(images[currentImg]),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                      if (counter > images.length) {
+                        counter = counter - 1;
+                      }
+                    });
+                  },
+                  child: const Text("Add")),
+              Column(
+                  children: List.generate(counter, (index) {
+                return Image.asset(images[index]);
+              })),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                   ElevatedButton(
-                      child: const Text("Change me"),
-                      onPressed: () {
-                        setState(() {
-                          currentImg = (currentImg + 1) % images.length;
-                        });
-                      }),
-                  Image.asset(images[currentImg]),
+                      style:
+                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      onPressed: () {},
+                      child: const Text("Register")),
                   ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green),
                       onPressed: () {
-                        setState(() {
-                          counter++;
-                          if (counter > images.length) {
-                            counter = counter - 1;
-                          }
-                        });
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginPage()),
+                        );
                       },
-                      child: const Text("Add")),
-                  Column(
-                      children: List.generate(counter, (index) {
-                    return Image.asset(images[index]);
-                  })),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red),
-                          onPressed: () {},
-                          child: const Text("Register")),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green),
-                          onPressed: () {},
-                          child: const Text("Login"))
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  )
-                ]),
+                      child: const Text("Login"))
+                ],
               ),
-            )));
+              const SizedBox(
+                height: 30,
+              )
+            ]),
+          ),
+        ));
   }
 }
